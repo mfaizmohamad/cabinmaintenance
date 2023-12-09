01bc9b68ae194168e4a30202d2c7e4d950e5c7ef
izleenfaqihah <izleenfaqihah@gmail.com>
2023-12-09 11:56:15 +0800

    main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './assets/component/Header/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/component/Header/Header.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import React from "react";
+import moment from "moment/moment";
+import "./Header.css";
+
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import "swiper/css/pagination";
+
+const Header = () => {
+  const [time, setTime] = useState(moment());
+  const displayDate = time.format("DD MMMM YYYY");
+
+  const setNextDate10 = moment().add(10, "days").calendar();
+  const setNextDate90 = moment().add(90, "days").calendar();
+  const setNextDate120 = moment().add(120, "days").calendar();
+  const setNextDate180 = moment().add(180, "days").calendar();
+
+  return (
+    <section className="top">
+      <h5>Cabin Maintenance</h5>
+      <h2>{displayDate}</h2>
+
+      <Swiper
+        className="container header__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide className="moment">
+          <h5>10 day from today</h5>
+          <h4>{setNextDate10}</h4>
+        </SwiperSlide>
+        <SwiperSlide className="moment">
+          <h5>90 day from today</h5>
+          <h4>{setNextDate90}</h4>
+        </SwiperSlide>
+        <SwiperSlide className="moment">
+          <h5>120 day from today</h5>
+          <h4>{setNextDate120}</h4>
+        </SwiperSlide>
+        <SwiperSlide className="moment">
+          <h5>180 day from today</h5>
+          <h4>{setNextDate180}</h4>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
+
+export default Header;
